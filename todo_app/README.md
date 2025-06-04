2f03bc5596718c033743b452d4f3d470e2666894
Timo Niemi <42733741+t-niemi@users.noreply.github.com>
2025-06-04 13:32:18 +0300

    Ex 1.2.

---
+++ todo_app/README.md
@@ -0,0 +1,3 @@
+## Log output program
+
+Run with `docker run --rm --init tniemi/dwk-todo-app`
---
+++ todo_app/src/app.ts
@@ -0,0 +1,10 @@
+import express from "express";
+
+const app = express();
+app.use(express.json());
+
+app.get("/ping", (_req, res) => {
+  res.send("pong");
+});
+
+export default app;
